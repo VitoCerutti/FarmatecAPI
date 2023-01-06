0c19cbc971c8ca79390699ac52e13dbaa104dd12
Vito <vitocerutti6@gmail.com>
2023-01-06 13:05:24 -0300

    Aquivos da API-Farmatec

---
+++ controllers/ControlePedidos.php
@@ -0,0 +1,163 @@
+<?php
+//Não apresentar tela com erros
+//ini_set('display_errors','0');
+
+use LDPA\Result;
+
+require_once("../databases/BdturmaConect.php");
+require_once("../config/SimpleRest.php");
+
+$page_key="";
+// termo extends pega emprestado o conteudo do arquivo 1 pra o 2
+class PedidosResHandler extends SimpleRest{
+
+    public function PedidosIncluir(){
+
+        if(isset($_POST["txtnomecliente"])) {
+
+            $nomecliente=$_POST["txtnomecliente"];
+            $canal=$_POST['txtcanal'];
+            $forma=$_POST['txtforma'];
+            $codfuncionario=$_POST['txtcodfuncionario'];
+            $datapgto=$_POST['txtdpgto'];
+            $dataenvi=$_POST['txtdenvio'];
+            $produto=$_POST['txtproduto'];
+
+            $query="CALL spInserirPedidos(:pnomecliente,:pcanal,:pforma,:pcodigofuncionario,:pdpgto,:pdenvio,:pproduto,@pnumeropedido)";
+            $array = array(":pnomecliente"=>"{$nomecliente}",":pcanal"=>"{$canal}",":pforma"=>"{$forma}",":pcodigofuncionario"=>"{$codfuncionario}",":pdpgto"=>"{$datapgto}",":pdenvio"=>"{$dataenvi}",":pproduto"=>"{$produto}");
+            $final = "SELECT @pnumeropedido as numeropedido";
+            
+            $dbcontroller = new BdturmaConect();
+
+            $rawData = $dbcontroller->executeProcedureOut($query,$array,$final);
+            
+            if(empty($rawData)){
+                $statusCode = 404;
+                $rawData = array('sucesso' => 0);
+            }
+            else{
+                $statusCode = 200;
+            }
+            
+            $requestContentType = $_POST['HTTP_ACCEPT'];
+            $this -> setHttpHeaders($requestContentType, $statusCode);
+            $result["RetornoDados"] = $rawData;
+
+            if(strpos($requestContentType,'application/Json') !== false){
+                $response = $this->encodeJson($result);
+                echo $response;
+            }
+
+        }
+
+    }
+
+    public function PedidosConsultar(){
+        if(isset($_POST["txtnumpedido"])){
+
+            $numpedido = $_POST["txtnumpedido"];
+            $cpf = $_POST["txtcpf"];
+
+        $query="CALL spConsultarPedidos(:npedido,:pcpf)";
+        $array = array(":npedido"=>"{$numpedido}",":pcpf"=>"{$cpf}");
+            $dbcontroller = new BdturmaConect ();
+            $rawData = $dbcontroller->executeProcedure($query,$array);
+            if(empty($rawData)){
+                $statusCode = 404;
+                $rawData = array('sucesso'=> 0);
+            }
+            else{
+                $statusCode = 200;
+            }
+            $requestContentType = $_POST['HTTP_ACCEPT'];
+            $this ->setHttpHeaders($requestContentType, $statusCode);
+
+            $Result["RetornoDados"] = $rawData;
+
+            if(strpos($requestContentType,'application/Json')!== false){
+                $response = $this -> encodeJson($Result);
+                echo $response;
+
+            }
+
+        }
+    }
+
+    public function PedidosConcluir(){
+        if(isset($_POST["txtnumpedido"])){
+
+            $numpedido = $_POST["txtnumpedido"];
+            $cpf = $_POST["txtcpf"];
+            $formaPgto = $_POST["txtforma"];
+
+        $query="CALL spConcluirPedidos(:npedido,:pcpf,:pforma)";
+        $array = array(":npedido"=>"{$numpedido}",":pcpf"=>"{$cpf}",,":pforma"=>"{$formaPgto}");
+            $dbcontroller = new BdturmaConect ();
+            $rawData = $dbcontroller->executeProcedure($query,$array);
+            if(empty($rawData)){
+                $statusCode = 404;
+                $rawData = array('sucesso'=> 0);
+            }
+            else{
+                $statusCode = 200;
+                $rawData = array('sucesso'=> 1);                
+            }
+            $requestContentType = $_POST['HTTP_ACCEPT'];
+            $this ->setHttpHeaders($requestContentType, $statusCode);
+
+            $Result["RetornoDados"] = $rawData;
+
+            if(strpos($requestContentType,'application/Json')!== false){
+                $response = $this -> encodeJson($Result);
+                echo $response;
+            }
+        }
+    }
+
+    public function encodeJson($responseData){
+        $JsonResponse = json_encode($responseData);
+        return $JsonResponse;
+    }
+}
+
+    
+    if(isset($_GET["page_key"])){
+        $page_key = $_GET["page_key"];
+    }
+
+    else{
+
+    if(isset($_POST["page_key"])){
+        $page_key = $_POST["page_key"];
+    }
+}
+
+
+if(isset($_POST["btnEnviar"])) {  
+    $page_key = "Incluir";
+    $_POST['HTTP_ACCEPT'] = "application/Json";
+    
+ }
+
+ if(isset($_POST["btnListar"])) {  
+    $page_key = "Consultar";
+    $_POST['HTTP_ACCEPT'] = "application/Json";
+ }
+
+switch($page_key){
+
+    case "Incluir":
+        $Pedidos = new PedidosResHandler();
+        $Pedidos -> PedidosIncluir();
+        break; 
+    case "Consultar":
+        $Pedidos = new PedidosResHandler();
+        $Pedidos -> PedidosConsultar();
+        break;
+    case "Concluir":
+        $Pedidos = new PedidosResHandler();
+        $Pedidos -> PedidosConcluir();
+        break;
+}
+
+?>
---
+++ controllers/ControleProdutos.php
@@ -0,0 +1,330 @@
+<?php
+//Não apresentar tela com erros
+//ini_set('display_errors','0');
+
+use LDPA\Result;
+
+require_once("../databases/BdturmaConect.php");
+require_once("../config/SimpleRest.php");
+
+$page_key="";
+
+class ProdutosResHandler extends SimpleRest{
+
+    public function ProdutosIncluir(){
+
+        if(isset($_POST["txtdescricao"])) {
+
+            $descricao=$_POST["txtdescricao"];
+            $marca=$_POST['txtmarca'];
+            $fornecedor=$_POST['txtfornecedor'];
+            $categoria=$_POST['txtcategoria'];
+            $unidade=$_POST['txtunidade'];
+            $estoquemin=$_POST['txtestoquemin'];
+            $preco=$_POST['txtpreco'];
+
+
+            $query="CALL spInserirProdutos(:pdescricao,:pmarca,:pfornecedor,:pcategoria,:punidade,:pestoquemin,:ppreco)";
+            $array = array(":pdescricao"=>"{$descricao}",":pmarca"=>"{$marca}",":pfornecedor"=>"{$fornecedor}",":pcategoria"=>"{$categoria}",":punidade"=>"{$unidade}",":pestoquemin"=>"{$estoquemin}",":ppreco"=>"{$preco}",);
+            //Instanciar a classe BdTurmaConnect
+             $dbcontroller = new BdturmaConect();
+
+             $rawData = $dbcontroller->executeProcedure($query,$array);
+            
+            //Verificar se o retorno esta "vazio"
+            if(empty($rawData)){
+                $statusCode = 404;
+                $rawData = array('sucesso' => 0);
+            }
+            else{
+                $statusCode = 200;
+            }
+            
+            $requestContentType = $_POST['HTTP_ACCEPT'];
+            $this -> setHttpHeaders($requestContentType, $statusCode);
+            $result["RetornoDados"] = $rawData;
+
+            if(strpos($requestContentType,'application/Json') !==false){
+                $response = $this->encodeJson($result);
+                echo $response;
+            }
+
+        }
+
+    }
+
+    public function EstoqueMovimentar(){
+
+        if(!empty($_POST["txtcodprod"])){
+
+            $status = $_POST["txtstatus"];
+            $produto = $_POST["txtcodprod"];
+            $qtde = $_POST["txtqtde"];
+            $descricao = $_POST["txtdesc"];
+        //Informar a Stored Produre e seus Parâmetros
+        $query="CALL spMovimentarEstoque(:pstatus,:pcodigoprod,:pqtde,:pdescricao)";
+        //Definir o conjunto de dados
+        $array = array(":pstatus"=>"{$status}",":pcodigoprod"=>"{$produto}",":pqtde"=>"{$qtde}",":pdescricao"=>"{$descricao}");
+            //Instanciar a classe BdTurmaConect
+            $dbcontroller = new BdturmaConect ();
+            //Chamar o método
+            $rawData = $dbcontroller->executeProcedure($query,$array);
+            //Verificar se o retorno está "Vazio"
+            if(empty($rawData)){
+                $statusCode = 404;
+                $rawData = array('sucesso'=> 0);
+            }
+            else{
+                $statusCode = 200;
+            }
+            $requestContentType = $_POST['HTTP_ACCEPT'];
+            $this ->setHttpHeaders($requestContentType, $statusCode);
+
+            $Result["RetornoDados"] = $rawData;
+
+            if(strpos($requestContentType,'application/Json')!== false){
+                $response = $this -> encodeJson($Result);
+                echo $response;
+
+            }
+
+        }
+
+    }
+
+    public function ProdutosConsultar(){
+
+        if(isset($_POST["txtcategoria"])){
+
+            $categoria = $_POST["txtcategoria"];
+        //Informar a Stored Produre e seus Parâmetros
+        $query="CALL spConsultarProdutos(:pcategoria)";
+        //Definir o conjunto de dados
+        $array = array(":pcategoria"=>"{$categoria}");
+            //Instanciar a classe BdTurmaConect
+            $dbcontroller = new BdturmaConect ();
+            //Chamar o método
+            $rawData = $dbcontroller->executeProcedure($query,$array);
+            //Verificar se o retorno está "Vazio"
+            if(empty($rawData)){
+                $statusCode = 404;
+                $rawData = array('sucesso'=> 0);
+            }
+            else{
+                $statusCode = 200;
+            }
+            $requestContentType = $_POST['HTTP_ACCEPT'];
+            $this ->setHttpHeaders($requestContentType, $statusCode);
+
+            $Result["RetornoDados"] = $rawData;
+
+            if(strpos($requestContentType,'application/Json')!== false){
+                $response = $this -> encodeJson($Result);
+                echo $response;
+            }
+        }
+    }
+
+    public function ProdutosConsultarcod(){
+
+        if(isset($_POST["txtcodprod"])){
+        
+        $codprod = $_POST["txtcodprod"];
+        $nomeprod = $_POST["txtdescricao"];
+
+        //Informar a Stored Produre e seus Parâmetros
+        $query="CALL spConsultarProdutoscod(:pcodprod,:pdescricao)";
+        //Definir o conjunto de dados
+        $array = array(":pcodprod"=>"{$codprod}",":pdescricao"=>"{$nomeprod}");
+            //Instanciar a classe BdTurmaConect
+            $dbcontroller = new BdturmaConect ();
+            //Chamar o método
+            $rawData = $dbcontroller->executeProcedure($query,$array);
+            //Verificar se o retorno está "Vazio"
+            if(empty($rawData)){
+                $statusCode = 404;
+                $rawData = array('sucesso'=> 0);
+            }
+            else{
+                $statusCode = 200;
+            }
+            $requestContentType = $_POST['HTTP_ACCEPT'];
+            $this ->setHttpHeaders($requestContentType, $statusCode);
+
+            $Result["RetornoDados"] = $rawData;
+
+            if(strpos($requestContentType,'application/Json')!== false){
+                $response = $this -> encodeJson($Result);
+                echo $response;
+            }
+        }
+    }
+
+    public function ProdutosConsultarWeb(){
+
+        if(isset($_POST["txtcodprod"])){
+        
+        $codprod = $_POST["txtcodprod"];
+
+        //Informar a Stored Produre e seus Parâmetros
+        $query="CALL spConsultarProdutoscod(:pcodprod,:pdescricao)";
+        //Definir o conjunto de dados
+        $array = array(":pcodprod"=>"{$codprod}");
+            //Instanciar a classe BdTurmaConect
+            $dbcontroller = new BdturmaConect ();
+            //Chamar o método
+            $rawData = $dbcontroller->executeProcedure($query,$array);
+            //Verificar se o retorno está "Vazio"
+            if(empty($rawData)){
+                $statusCode = 404;
+                $rawData = array('sucesso'=> 0);
+            }
+            else{
+                $statusCode = 200;
+            }
+            $requestContentType = $_POST['HTTP_ACCEPT'];
+            $this ->setHttpHeaders($requestContentType, $statusCode);
+
+            $Result["RetornoDados"] = $rawData;
+
+            if(strpos($requestContentType,'application/Json')!== false){
+                $response = $this -> encodeJson($Result);
+                echo $response;
+            }
+        }
+    }
+
+    public function AtualizarPreco(){
+            if(!empty($_POST["txtcodprod"])){
+
+            $codprod = $_POST["txtcodprod"];
+            $preco = $_POST["txtpreco"];
+        //Informar a Stored Produre e seus Parâmetros
+        $query="CALL spAtualizarPreco(:pcodprod,:ppreco)";
+        //Definir o conjunto de dados
+        $array = array(":pcodprod"=>"{$codprod}",":ppreco"=>"{$preco}");
+            //Instanciar a classe BdTurmaConect
+            $dbcontroller = new BdturmaConect ();
+            //Chamar o método
+            $rawData = $dbcontroller->executeProcedure($query,$array);
+            //Verificar se o retorno está "Vazio"
+            if(empty($rawData)){
+                $statusCode = 404;
+                $rawData = array('sucesso'=> 0);
+            }
+            else{
+                $statusCode = 200;
+                $rawData = array('sucesso'=> 1);
+            }
+            $requestContentType = $_POST['HTTP_ACCEPT'];
+            $this ->setHttpHeaders($requestContentType, $statusCode);
+
+            $Result["RetornoDados"] = $rawData;
+
+            if(strpos($requestContentType,'application/Json')!== false){
+                $response = $this -> encodeJson($Result);
+                echo $response;
+
+            }
+
+        }
+    }
+
+    public function ProdutosRetirar(){
+        if(isset($_POST["txtnumpedido"])){
+
+            $numpedido = $_POST["txtnumpedido"];
+            $cpf = $_POST["txtcpf"];
+
+        $query="CALL spRetirarProdutos(:npedido,:pcpf)";
+        $array = array(":npedido"=>"{$numpedido}",":pcpf"=>"{$cpf}");
+            $dbcontroller = new BdturmaConect ();
+            $rawData = $dbcontroller->executeProcedure($query,$array);
+            if(empty($rawData)){
+                $statusCode = 404;
+                $rawData = array('sucesso'=> 0);
+            }
+            else{
+                $statusCode = 200;
+                $rawData = array('sucesso'=> 1);
+            }
+            $requestContentType = $_POST['HTTP_ACCEPT'];
+            $this ->setHttpHeaders($requestContentType, $statusCode);
+
+            $Result["RetornoDados"] = $rawData;
+
+            if(strpos($requestContentType,'application/Json')!== false){
+                $response = $this -> encodeJson($Result);
+                echo $response;
+
+            }
+
+        }
+    }
+
+    public function encodeJson($responseData){
+        $JsonResponse = json_encode($responseData);
+        return $JsonResponse;
+    }
+}
+
+    if(isset($_GET["page_key"])){
+        $page_key = $_GET["page_key"];
+    }
+
+    else{
+
+    if(isset($_POST["page_key"])){
+        $page_key = $_POST["page_key"];
+    }
+}
+
+
+if(isset($_POST["btnEnviar"])) {  
+    $page_key = "Incluir";
+    $_POST['HTTP_ACCEPT'] = "application/Json";
+    
+ }
+
+ if(isset($_POST["btnListar"])) {  
+    $page_key = "Consultar";
+    $_POST['HTTP_ACCEPT'] = "application/Json";
+ }
+
+switch($page_key){
+
+    case "Incluir":
+        $Produtos = new ProdutosResHandler();
+        $Produtos -> ProdutosIncluir();
+        break;
+
+    case "Movimentar":
+        $Produtos = new ProdutosResHandler();
+        $Produtos -> EstoqueMovimentar();
+        break;
+
+    case "Consultar":
+        $Produtos = new ProdutosResHandler();
+        $Produtos -> ProdutosConsultar();
+        break;  
+
+    case "Atualizar":
+        $Produtos = new ProdutosResHandler();
+        $Produtos -> AtualizarPreco();
+        break;
+    case "ConsultarCod":
+        $Produtos = new ProdutosResHandler();
+        $Produtos -> ProdutosConsultarcod();
+        break;
+    case "ConsultarWeb":
+        $Produtos = new ProdutosResHandler();
+        $Produtos -> ProdutosConsultarWeb();
+        break;
+    case "Retirar":
+        $Produtos = new ProdutosResHandler();
+        $Produtos -> ProdutosRetirar();
+        break;
+
+}
+
+?>
